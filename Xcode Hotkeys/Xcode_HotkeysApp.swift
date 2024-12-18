c5271005eb0560622a2bead850cac62caf36a846
prinonbhowmik <prinonarnob@gmail.com>
2024-12-19 02:49:35 +0600

    first commit

---
+++ Xcode Hotkeys/Xcode_HotkeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_HotkeysApp.swift
+//  Xcode Hotkeys
+//
+//  Created by Prinon bhowmik on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_HotkeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
